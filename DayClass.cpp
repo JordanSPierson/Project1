f0a0de5539c29c65a7bf2296d52088c15c960c17
JordanSPierson <43154073+JordanSPierson@users.noreply.github.com>
2018-10-18 10:35:48 -0600

    Change from day to data class other changes
    simplifications and additions of accessor methods

--- DayClass.cpp
+++ DayClass.cpp
@@ -1,75 +1,108 @@
 // Blood Sugar Data class
 
-class Day
+
+
+class Data
 {
 private:
-	float daySum,
-		daySumOFCounter,
-		dayMax,
-		dayMin,
-		dayCount,
-		weekSum,
-		weekOFCounter,
-		weekMax,
-		weekMin,
-		weekCount;
+	float sum,
+		sumOFCounter,
+		max,
+		min,
+		count;
 
 public:
-	//inputNum is used when a number is added, and calls other
-	//functions as appropriate
-	void inputNum(float input);
+	
+	//Functions that add or increment variables recorded. addSum also calls maxCheck and minCheck.
+	void addSum(float input);
+	void maxCheck(float input);
+	void minCheck(float input);
 	//
-	//Functions that add or increment variables recorded for the day
-	void addDaySum(float input);
-	void dayMaxCheck(float input);
-	void dayMinCheck(float input);
-	void incDayCount(void);
-	//
-	//Functions that add or increment variables recorded for the week
+	//Accessor Functions
+	string returnSum(void);
+	float returnMax(void);
+	float returnMin(void);
+	float returnCount(void);
 };
 
 void Day::inputNum(float input)
 {
-	addDaySum(input);
-	dayMaxCheck(input);
-	dayMinCheck(input);
+	addSum(input);
+	maxCheck(input);
+	minCheck(input);
 }
-void Day::addDaySum(float input)
+
+void Data::addSum(float input)
 {
 	//check to see if valid, highest recorded blood sugar was 147.6 mmol/L
-	if (input <= 150)
+	if (input <= 150 && input > 0)
 	{
 		//checks to see if the daySum is going to overflow
-		if (daySum + input >= 1000000)
+		if (sum + input >= 1000000)
 		{
 			//Sets day sum to new number & increments the day sum counter
-			daySum = daySum + input - 1000000;
-			daySumOFCounter++;
+			sum = sum + input - 1000000;
+			sumOFCounter++;
 		}
-
+		else
+		{
+			sum = sum + input;
+		}
+		count++;
+		maxCheck(input);
+		minCheck(input);
 	}
 }
 
-void Day::dayMaxCheck(float input)
+void Data::maxCheck(float input)
 {
-	if (input > dayMax)
+	if (input > max)
 	{
-		dayMax = input;
+		max = input;
 	}
 }
 
-void Day::dayMinCheck(float input)
+void Data::minCheck(float input)
 {
-	if (input < dayMin)
+	if (input < min)
 	{
-		dayMin = input;
+		min = input;
 	}
 }
 
-void Day::incDayCount()
+//Accessor Functions
+
+string Data::returnSum()
 {
-	dayCount++;
+	string sSum;
+	if(sumOFCounter > 0)
+	{
+		sSum = to_string(sumOFCounter) + " x 1,000,000 + " + to_string(sum);
+	}
+	else
+	{
+		sSum = to_string(sum);
+	}
+	//find way of storing both the sum and sum overflow variables in string!	
+	return sSum;
+		
 }
+
+float Data::returnMax()
+{
+	return max;
+}
+
+float Data::returnMin()
+{
+	return min;
+}
+
+float Data::returnCount()
+{
+	return count;
+}
+
 //----------------------------------------------------
 //Current idea of how to go on from here:
 //
